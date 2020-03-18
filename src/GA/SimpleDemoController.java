7483ec405727a2aaa8b318d315cf6c61c8fc07f7
Ada Meceffeuk <46904388+averroes96@users.noreply.github.com>
2020-03-18 21:52:54 +0100

    V1.1
    Added slider to specify the maximum number of generations and a toggle button to chose whether to show the genetic poll or not

--- src/GA/SimpleDemoController.java
+++ src/GA/SimpleDemoController.java
@@ -6,32 +6,40 @@
 package GA;
 
 import Include.Individual;
+import Include.Init;
 import Include.Population;
+import Include.SpecialAlert;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXSlider;
 import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXToggleButton;
 import java.net.URL;
+import java.text.NumberFormat;
 import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 
 /**
  *
  * @author user
  */
-public class SimpleDemoController implements Initializable {
+public class SimpleDemoController implements Initializable,Init {
     
     @FXML public JFXSlider numberOfGenes;
-    @FXML public JFXSlider numberOfIndividuals;
+    @FXML public JFXSlider numberOfIndividuals,genSlider;
     @FXML public JFXButton generator;
     @FXML public JFXTextArea result;
-    @FXML public Label nbrIndividuals,nbrGenes;
+    @FXML public Label nbrIndividuals,nbrGenes,genCount;
+    @FXML public JFXToggleButton showPoll;
     
     private Population population;
     private Individual fittest,secondFittest;
     private int generationCount;
+    private final SpecialAlert alert = new SpecialAlert();
+    private static int nbrTries;
 
 	//Selection
 	void selection() {
@@ -110,22 +118,17 @@ public class SimpleDemoController implements Initializable {
             
                 String geneticPool = "";
             
-		geneticPool += "\n==Genetic Pool==\n";
+		geneticPool += "\n=============== Genetic Pool ================\n";
 		int increment=0;
 		for (Individual individual:individuals) {
 			geneticPool += "> Individual  "+increment+" | "+ individual.genesToString()+" |\n";
 			increment++;
 		}
-		geneticPool += "\n================\n";
+		geneticPool += "\n================================\n";
                 
                 return geneticPool;
 	}    
-    
-    
-        /*public SimpleDemoController() {
-		this.population = new Population((int)numberOfIndividuals.getValue(), (int)numberOfGenes.getValue());
-		this.generationCount = 0;
-    }  */  
+
     
         public void generateResults(){
             
@@ -134,19 +137,24 @@ public class SimpleDemoController implements Initializable {
         population = new Population((int)numberOfIndividuals.getValue(), (int)numberOfGenes.getValue());
         generationCount = 0;
         
-		result.appendText("Population of " + population.getPopSize() + " individual(s), each Individual with : " + numberOfGenes.getValue() + " gene(s).\n");
+        nbrTries++;
+		result.appendText("Population of " + population.getPopSize() + " individual(s), each Individual with : " + (int)numberOfGenes.getValue() + " gene(s).\n");
 
 		//Calculate fitness of each individual
 		population.calculateFitness();
 
 		result.appendText("\nGeneration: " + generationCount + " Fittest: " + population.getFittestScore());
-		//show genetic pool
-		result.appendText(showGeneticPool(population.getIndividuals()));
+                
+                if(showPoll.isSelected()){
+                    //show genetic pool
+                    result.appendText(showGeneticPool(population.getIndividuals()));
+                }
 
 		//While population gets an individual with maximum fitness
-		while (population.getFittestScore() < numberOfGenes.getValue()) {
+		while (population.getFittestScore() < numberOfGenes.getValue() && generationCount < (int)genSlider.getValue()){
+                    
 			++generationCount;
-
+                        
 			//Do selection
 			selection();
 
@@ -167,31 +175,59 @@ public class SimpleDemoController implements Initializable {
 			population.calculateFitness();
 
 			result.appendText("\nGeneration: " + generationCount + " Fittest score: " + population.getFittestScore());
-            
-			//show genetic pool
-			result.appendText(showGeneticPool(population.getIndividuals()));
+                        
+                        if(showPoll.isSelected()){
+                            //show genetic pool
+                            result.appendText(showGeneticPool(population.getIndividuals()));
+                        }
 		}
+                
+                if(generationCount == (int)genSlider.getValue()){
+                    
+                    
+                    
+                    if(nbrTries >= 10){
+                        alert.show(MAX_GEN_ERROR, MAX_GEN_ERROR_MESSAGE, Alert.AlertType.ERROR, false);
+                        nbrTries = 0;
+                    }
+                    else
+                        generateResults();
+                }
+                else{
+                    
+                    result.appendText("\n\n> Solution found in generation " + generationCount);
+                    result.appendText("\n> Index of winner Individual: "+population.getFittestIndex());
+                    result.appendText("\n> Fitness: "+population.getFittestScore());
+                    result.appendText("\n> Genes: ");
 
-		result.appendText("\nSolution found in generation " + generationCount);
-		result.appendText("\nIndex of winner Individual: "+population.getFittestIndex());
-		result.appendText("\nFitness: "+population.getFittestScore());
-		result.appendText("\nGenes: ");
-		for (int i = 0; i < numberOfGenes.getValue(); i++) {
-			result.appendText(String.valueOf(population.selectFittest().getGenes()[i]));
-		}
+                    for (int i = 0; i < numberOfGenes.getValue(); i++) {
+                            result.appendText(String.valueOf(population.selectFittest().getGenes()[i]));
+                    }
+                    
+                    result.appendText("\n> Number of tries : " + nbrTries);
+                    
+                    nbrTries = 0;
+                    
+                }
 
         
     }
+        
+        public void initSlider(JFXSlider slider, Label sliderLabel){
+            
+            slider.valueProperty().addListener((obs,oldVal,newVal)->{
+                slider.setValue(newVal.intValue());
+            });
+
+            sliderLabel.textProperty().bindBidirectional(slider.valueProperty(), NumberFormat.getIntegerInstance());            
+        }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
-        numberOfGenes.valueProperty().addListener((obs,oldVal,newVal)->{
-            numberOfGenes.setValue(newVal.intValue());
-        });
-        numberOfIndividuals.valueProperty().addListener((obs,oldVal,newVal)->{
-            numberOfIndividuals.setValue(newVal.intValue());
-        });
+        initSlider(numberOfGenes, nbrGenes);
+        initSlider(numberOfIndividuals, nbrIndividuals);
+        initSlider(genSlider, genCount);      
             
         generator.setOnAction(Action -> {
             generateResults();
---
+++ src/Include/Init.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Include;
+
+/**
+ *
+ * @author user
+ */
+public interface Init {
+    
+    String MAX_GEN_ERROR = "Maximum generations !";
+    
+    String MAX_GEN_ERROR_MESSAGE = "The number of maximum generations specified was reached before finding a solution ! Try another set of parameters" ;
+    
+}
